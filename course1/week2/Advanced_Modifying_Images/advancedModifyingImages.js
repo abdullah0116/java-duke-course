878c058c802dacad5621454f93e7573efcb6c43b
Abdullah Kareem <akasak56@gmail.com>
2018-08-17 23:18:54 -0600

    part 2 for homework.

--- course1/week2/Advanced_Modifying_Images/advancedModifyingImages.js
+++ course1/week2/Advanced_Modifying_Images/advancedModifyingImages.js
@@ -17,3 +17,22 @@ print(outputImage);
 
 
 // Part 2
+var img = new SimpleImage(200,200);
+for (var px of img.values()){
+  var x = px.getX();
+  var y = px.getY();
+  if (x < img.getWidth()/2){
+    px.setRed(255);
+  }
+  if (y>img.getHeight()/2){
+    px.setBlue(255);
+  }
+  else {
+    px.setGreen(255);
+  }
+
+  if (px.getRed() == 255 && px.getGreen() == 255) {
+      px.setGreen(0);
+  }
+}
+print (img);
