812af65653ccd50aa4fc443b6b3bed634cfc5d78
Abdullah Kareem <akasak56@gmail.com>
2018-08-20 19:50:06 -0600

    3rd part of homework.

--- course1/week3/Buttons_And_Divs/main.js
+++ course1/week3/Buttons_And_Divs/main.js
@@ -3,5 +3,20 @@ function male() {
 }
 
 function female() {
-  alert('Your gender has been updated to Female.')
+  alert('Your gender has been updated to Female.');
+}
+
+//var conformationAnswer = confirm('confirm this');
+
+function conformationText() {
+  var confirmSTAT = confirm('Is the data you have selected correct?');
+  var response;
+
+  if (confirmSTAT == true) {
+    response = "you have confirmed the data";
+  } else {
+    response = "You have not confirmed the data";
+  }
+
+  document.getElementById("confirmedData").innerHTML = response;
 }
