21931f531de7900fb9bc233b940f9aa7bdf503f6
Abdullah Kareem <akasak56@gmail.com>
2018-08-24 00:12:15 -0600

    added drawSqaure function.

--- course1/week3/Inputs_and_Events/main.js
+++ course1/week3/Inputs_and_Events/main.js
@@ -8,6 +8,23 @@ function colorInput() {
   var inputColor = document.getElementById("colorPicker");
   var color = inputColor.value;
   canvas.style.backgroundColor = color;
+}
 
+function drawSquare() {
+  // instance variable of the canvas
+  var canvas = document.getElementById("mainCanvas");
+  // variable for the context of the canvas variable
+  var canvasCTX = canvas.getContext("2d");
 
+  // instance variable of the slider
+  var slider = document.getElementById("squareSize");
+  // gets the value of the slider number
+  var size = slider.value;
+
+  canvasCTX.clearRect(0,0, canvas.width, canvas.height)
+
+  // sets the color of context to purple
+  canvasCTX.fillStyle = "purple";
+  //
+  canvasCTX.fillRect(0,0, size, size);
 }
