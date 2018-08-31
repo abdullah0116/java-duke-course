6889fad7999aa6f8f66b4506cb7ca6bf136bd46e
Abdullah Kareem <akasak56@gmail.com>
2018-08-30 20:38:32 -0600

    added makeRed() function.

--- course1/week4/Mini_Project_2/main.js
+++ course1/week4/Mini_Project_2/main.js
@@ -1,4 +1,4 @@
-var image;
+var redImage;
 var cleanImage;
 var leftCanvas;
 var rightCanvas;
@@ -7,10 +7,26 @@ function upload() {
   leftCanvas = document.getElementById("leftCanvas");
   var rawImage = document.getElementById("imageUpload");
   cleanImage = new SimpleImage(rawImage);
-  image = new SimpleImage(rawImage);
+  redImage = new SimpleImage(rawImage);
   cleanImage.drawTo(leftCanvas);
 }
 
 function makeRed() {
-
+  rightCanvas = document.getElementById("rightCanvas");
+  for (var pixel of redImage.values()) {
+    var average = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
+    if (average < 128) {
+      var newRed = average * 2;
+      pixel.setRed(newRed);
+      pixel.setGreen(0);
+      pixel.setBlue(0);
+    } else {
+      pixel.setRed(0);
+      var newGreen = (average * 2) - 255;
+      pixel.setGreen(newGreen);
+      var newBlue = (average * 2) - 255;
+      pixel.setBlue(newBlue);
+    }
+  }
+  redImage.drawTo(rightCanvas);
 }
