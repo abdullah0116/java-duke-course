c7fccc0960daa91f10f33dce72ebf09708ae91d2
Abdullah Kareem <akasak56@gmail.com>
2018-08-31 07:17:01 -0600

    new function

--- course1/week4/Mini_Project_2/main.js
+++ course1/week4/Mini_Project_2/main.js
@@ -1,4 +1,5 @@
 var redImage;
+var blueImage;
 var cleanImage;
 var leftCanvas;
 var rightCanvas;
@@ -6,6 +7,7 @@ var rightCanvas;
 function upload() {
   leftCanvas = document.getElementById("leftCanvas");
   var rawImage = document.getElementById("imageUpload");
+  blueImage = new SimpleImage(rawImage);
   cleanImage = new SimpleImage(rawImage);
   redImage = new SimpleImage(rawImage);
   cleanImage.drawTo(leftCanvas);
@@ -29,4 +31,25 @@ function makeRed() {
     }
   }
   redImage.drawTo(rightCanvas);
+}
+
+function makeBlue() {
+  for (var pixel of blueImage.values()) {
+    var average = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
+    if (average < 128) {
+      var newBlue = average * 2;
+      pixel.setRed(0);
+      pixel.setGreen(0);
+      pixel.setBlue(newBlue);
+    } else {
+      var newRed = (average * 2) - 255;
+      pixel.setRed(newRed);
+      var newGreen = (average * 2) - 255;
+      pixel.setGreen(newGreen);
+      pixel.setBlue(0);
+    }
+  }
+  var ctx = rightCanvas.getContext("2d");
+  ctx.clearRect(0, 0, rightCanvas.width, rightCanvas.height);
+  blueImage.drawTo(rightCanvas);
 }
