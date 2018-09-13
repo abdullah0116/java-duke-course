0d67583dc4f9ac174e18e741cf214ffee9fe22eb
Abdullah Kareem <akasak56@gmail.com>
2018-09-12 21:00:42 -0600

    starting src code for project.

--- course2/week1/hello/HelloWorld.java
+++ course2/week1/hello/HelloWorld.java
@@ -1,7 +1,7 @@
 import edu.duke.*;
 
 public class HelloWorld {
-	public static void main() {
+	public void runHello () {
 		FileResource res = new FileResource("hello_unicode.txt");
 		for (String line : res.lines()) {
 			System.out.println(line);
---
+++ course2/week1/perimeter/PerimeterRunner.java
@@ -0,0 +1,27 @@
+import edu.duke.*;
+
+public class PerimeterRunner {
+    public double getPerimeter(Shape s) {
+        // start with totalPerim = 0;
+        double totalPerim = 0;
+        // start with prevPT = the last point
+        Point prevPT = s.getLastPoint();
+        // for each point currPT in the shape 
+            // find the distance from prevPT pt to currPT set it to currDist 
+            // update totalPerim to be totalPerim + (plus) currDist 
+            // update prevPT to be currPT
+        // totalPerim is the answer 
+    }
+    
+    public void testPerimeter() {
+        FileResource fr = new FileResource();
+        Shape s = new Shape(fr);
+        double length = getPerimeter(s);
+        System.out.println("Perimter =" + length);
+    }
+    
+    public static void main(String[] args) {
+        PerimeterRunner pr = new PerimeterRunner();
+        pr.testPerimeter();
+    }
+}
