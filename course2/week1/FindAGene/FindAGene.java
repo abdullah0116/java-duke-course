816122e4b38609832fb5425d9a9216961a947d87
Abdullah Kareem <akasak56@gmail.com>
2018-09-19 07:02:18 -0600

    more test cases.

--- course2/week1/FindAGene/FindAGene.java
+++ course2/week1/FindAGene/FindAGene.java
@@ -9,8 +9,16 @@ public class FindAGene {
     public String FindGeneSimple(String dna) {
         // start codon 
         int startCodon = dna.indexOf("ATG");
+        if (startCodon == -1) {
+            System.out.println("There is no start of DNA");
+            return "";
+        }    
         // end codon 
         int endCodon = dna.indexOf("TAA", startCodon+3);
+        if (endCodon == -1) {
+            System.out.println("There is no end the DNA strand");
+            return "";
+        }
         String result = dna.substring(startCodon, endCodon+3);
         return result;
     }
@@ -23,6 +31,16 @@ public class FindAGene {
         System.out.println("Your Gene is: " + gene);
         
         dna = "ATTATGEEWLKRWATTTAA";
+        System.out.println("DNA strand is: " + dna);
+        gene = FindGeneSimple(dna);
+        System.out.println("Your Gene is: " + gene);
+        
+        dna = "ATBGTAA";
+        System.out.println("DNA strand is: " + dna);
+        gene = FindGeneSimple(dna);
+        System.out.println("Your Gene is: " + gene);
+        
+        dna = "ATGGTBA";
         System.out.println("DNA strand is: " + dna);
         gene = FindGeneSimple(dna);
         System.out.println("Your Gene is: " + gene);
