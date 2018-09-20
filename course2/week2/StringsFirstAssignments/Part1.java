00fe0fd4d995480ae6dfd580ed4a8e90d5619ecf
Abdullah Kareem <akasak56@gmail.com>
2018-09-19 20:23:55 -0600

    finished part #1.

--- course2/week2/StringsFirstAssignments/Part1.java
+++ course2/week2/StringsFirstAssignments/Part1.java
@@ -6,5 +6,37 @@
  * @version (a version number or a date)
  */
 public class Part1 {
+    public String findSimpleGene(String dna) {
+        //Finds the index position of the start codon “ATG”. 
+        int startCodon = dna.indexOf("ATG");
+        //If there is no “ATG”, return the empty string.
+        if (startCodon == -1) {
+            return "";
+        }
+        
+        //Finds the index position of the first stop codon “TAA” appearing after
+        // the “ATG” that was found. 
+        int stopCodon = dna.indexOf("TAA", startCodon+3);
+        //If there is no such “TAA”, return the empty string.
+        if (stopCodon == -1) {
+            return "";
+        }
+        
+        String result = dna.substring(startCodon, stopCodon+3);
+        //If the length of the substring between the “ATG” and “TAA” is a 
+        //multiple of 3, then return the substring that starts with that 
+        if((startCodon + stopCodon) % 3 == 0) {
+            return result;
+        }
+        //“ATG” and ends with that “TAA”.
+
+        return "";
+    }
     
+    public void SimpleGeneTester() {
+        String rawGene = "JKLDJSLKDATGGKGIKCTAAJKDJSDLKJSD";
+        System.out.println("The Raw Gene is: " + rawGene);
+        String DNA = findSimpleGene(rawGene);
+        System.out.println("The DNA is: " + DNA); 
+    }
 }
