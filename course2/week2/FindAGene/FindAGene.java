c1f3477b8fcdf1763bd6c11193d964047b4cd835
Abdullah Kareem <akasak56@gmail.com>
2018-09-19 18:51:11 -0600

    new file dirctory.

---
+++ course2/week2/FindAGene/FindAGene.java
@@ -0,0 +1,49 @@
+
+/**
+ * Write a description of FindAGene here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FindAGene {
+    public String FindGeneSimple(String dna) {
+        // start codon 
+        int startCodon = dna.indexOf("ATG");
+        if (startCodon == -1) {
+            System.out.println("There is no start of DNA");
+            return "";
+        }    
+        // end codon 
+        int endCodon = dna.indexOf("TAA", startCodon+3);
+        String result = dna.substring(startCodon, endCodon+3);
+        if (endCodon == -1) {
+            System.out.println("There is no end the DNA strand");
+            return "";
+        }
+   
+        return result;
+    }
+    
+    // this method has some tests for Find Gene Simple method
+    public void testFindGene() {
+        String dna = "AATGCGTAATATGGT";
+        System.out.println("DNA strand is: " + dna);
+        String gene = FindGeneSimple(dna);
+        System.out.println("Your Gene is: " + gene);
+        
+        dna = "ATTATGEEWLKRWATTTAA";
+        System.out.println("DNA strand is: " + dna);
+        gene = FindGeneSimple(dna);
+        System.out.println("Your Gene is: " + gene);
+        
+        dna = "ATBGTAA";
+        System.out.println("DNA strand is: " + dna);
+        gene = FindGeneSimple(dna);
+        System.out.println("Your Gene is: " + gene);
+        
+        dna = "ATGGTBA";
+        System.out.println("DNA strand is: " + dna);
+        gene = FindGeneSimple(dna);
+        System.out.println("Your Gene is: " + gene);
+    }
+}
---
+++ course2/week2/StringsFirstAssignments/Part1.java
@@ -0,0 +1,10 @@
+
+/**
+ * Write a description of Part1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Part1 {
+    
+}
