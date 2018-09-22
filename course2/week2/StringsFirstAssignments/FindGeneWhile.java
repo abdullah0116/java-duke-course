6a32a4415bf834aeb55cc9e1fec2d2c22fdf2242
Abdullah Kareem <akasak56@gmail.com>
2018-09-22 10:59:07 -0600

    looks for mulitiple taa strings now.

---
+++ course2/week2/StringsFirstAssignments/FindGeneWhile.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of FindGeneWhile here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FindGeneWhile {
+    public String findGene(String dna) {
+        // I found the first occurrence of “ATG” — startIndex 
+        int startIndex = dna.indexOf("ATG"); 
+        // find the “TAA” starting from (startIndex + 3) call this result — currIndex 
+        int currIndex = dna.indexOf("TAA", startIndex + 3);
+        // as long as currIndex is not equal to -1
+        while(currIndex != -1) {
+            // check if (currIndex - startIndex) is a multiple of 3 
+            if ((currIndex - startIndex) % 3 == 0) {
+                // if so the text between start index and currIndex + 3 is 	the answer 
+                return dna.substring(startIndex, currIndex+3);
+            } else {
+                //  if not update currIndex to the index of the next “TAA” 	starting from (currIndex + 1)
+                currIndex = dna.indexOf("TAA", currIndex + 1);
+            } 
+        }
+        //answer is an empty string 
+        return "";
+    }
+    
+    public void findGeneTester() {
+        String dna  = "CGATGGTAAATAAGCCTAAGCTATAA";
+        System.out.println("The DNA strand: " + dna);
+        String gene = findGene(dna);
+        System.out.println("The Gene strand is: " + gene);
+    }
+}
