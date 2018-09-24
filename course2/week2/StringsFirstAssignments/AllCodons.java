473749113c48a1652672e5e55b75b91191866745
Abdullah Kareem <akasak56@gmail.com>
2018-09-24 06:10:28 -0600

    new three method findGene method.

--- course2/week2/StringsFirstAssignments/AllCodons.java
+++ course2/week2/StringsFirstAssignments/AllCodons.java
@@ -29,12 +29,63 @@ public class AllCodons {
     
     public String findGene(String dna) {
         // find first occurrence of "ATG" call its index startIndex 
+        int startIndex = dna.indexOf("ATG");
         // if startIndex is -1 then your answer is the empty string 
+        if(startIndex == -1){
+            return "";
+        }
         // findStopCodon(dnaString, startIndex, "TAA") and call the result taaIndex
+        int taaIndex = findStopCodon(dna, startIndex, "TAA");
         // findStopCodon(dnaString, startIndex, "TAG") and call the result tagIndex
+        int tagIndex = findStopCodon(dna, startIndex, "TAG");
         // findStopCodon(dnaString, startIndex, "TGA") and call the result tgaIndex
+        int tgaIndex = findStopCodon(dna, startIndex, "TGA");
         // take the smallest of taaIndex, tgaIndex, and tagIndex: call it minIndex
+        int firstMin = Math.min(taaIndex, tagIndex);
+        int minIndex = Math.min(firstMin, tgaIndex);
+        
+        if(minIndex == dna.length()){
+            return "";
+        }
         // your answer is the text from startindex to minindex + 3
-        return "";
+        return dna.substring(startIndex, minIndex + 3);
+    }
+    
+    public void testFindGene() {
+        String dnaTAA = "xxxyyyzzzTAAxxxyyyzzzTAAxx";
+        String dnaTAG = "xxxyyyzzzTAGxxxyyyzzzTAGxx";
+        String dnaTGA = "xxxyyyzzzTGAxxxyyyzzzTGAxx";
+        
+        System.out.println("Gene of TAA: " + findStopCodon(dnaTAA, 0, "TAA"));
+        System.out.println("Gene of TAG: " + findStopCodon(dnaTAG, 0, "TAG"));
+        System.out.println("Gene Of TGA: " + findStopCodon(dnaTGA, 0, "TGA"));
+    }
+    
+    public void testFindStop() {
+        String dna = "xxxyyyzzzTAAxxxyyyzzzTAAxx";
+        int dex = findStopCodon(dna, 0, "TAA");
+        if (dex != 9) {
+            System.out.println("error on line 9!");
+        }
+        
+        dex = findStopCodon(dna, 9, "TAA");
+        
+        if (dex != 21){
+            System.out.println("error on line 21!");
+        }
+        
+        dex = findStopCodon(dna, 1, "TAA");
+        
+        if (dex != 26){
+            System.out.println("error on line 26!");
+        }
+        
+        dex = findStopCodon(dna, 0, "TAG");
+        
+        if (dex != 26){
+            System.out.println("error on line 26!");
+        }
+        
+        System.out.println("All tests are complete.");
     }
 }
