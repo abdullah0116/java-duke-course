1ca7aa183723e376e068b7790c0251735b90b7a8
Abdullah Kareem <akasak56@gmail.com>
2018-10-12 01:43:52 -0600

    new replaceVowel() method and new tester method for it as well.

--- course3/week1/CaesarCipher/WordPlay.java
+++ course3/week1/CaesarCipher/WordPlay.java
@@ -1,21 +1,36 @@
 
 public class WordPlay {
-    public boolean isVowel(char ch) {
-        boolean result = false;
-        char[] chars = {'a', 'e', 'i', 'o', 'u'};
+    public String replaceVowels(String phrase, char ch) {
+        String phraseLowCase = phrase.toLowerCase();
+        StringBuilder result = new StringBuilder(phraseLowCase);
         
-        for (int i = 0; i < chars.length; i++) {
-            if (chars[i] == ch) {
-                return true;
+        for (int i = 0; i < result.length(); i++) {
+            if (isVowel(result.charAt(i)) == true) {
+                result.setCharAt(i, ch);
             }
         }
         
+        return result.toString();
+    }
+    
+    public boolean isVowel(char ch) {
+        boolean result = false;
+        char[] chars = {'a', 'e', 'i', 'o', 'u'};      
+        for (int i = 0; i < chars.length; i++) {
+            char chLow = Character.toLowerCase(ch);
+            if (chars[i] == chLow) {
+                return true;
+            }
+        }       
         return result;
     }
     
     //             *** TESTER METHOD ***
+    public void testReplaceVowels() {
+        System.out.println(replaceVowels("Hello World", '*'));
+    }
     
     public void testIsVowel() {
-        System.out.println(isVowel('a'));
+        System.out.println(isVowel('A'));
     }
 }
