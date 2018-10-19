6a9961ef12a54e4a524b6792053acba67183ba6c
Abdullah Kareem <akasak56@gmail.com>
2018-10-19 00:38:14 -0600

    new emphasize method with its tester method finished

--- course3/week1/CaesarCipher/WordPlay.java
+++ course3/week1/CaesarCipher/WordPlay.java
@@ -1,5 +1,22 @@
 
 public class WordPlay {
+    public String emphasize(String phrase, char ch) {
+        StringBuilder result = new StringBuilder(phrase);
+        char[] phraseArray = phrase.toCharArray();
+        for (int i = 0; i < phraseArray.length; i++) {
+            if (phraseArray[i] == ch) {
+                int indexChar = i;
+                if (i % 2 == 0) {
+                    result.setCharAt(indexChar, '*');
+                } else if (i % 2 != 0) {
+                    result.setCharAt(indexChar, '+');
+                }
+            }
+        }
+        
+        return result.toString();
+    }
+    
     public String replaceVowels(String phrase, char ch) {
         String phraseLowCase = phrase.toLowerCase();
         StringBuilder result = new StringBuilder(phraseLowCase);
@@ -12,7 +29,7 @@ public class WordPlay {
         
         return result.toString();
     }
-    
+
     public boolean isVowel(char ch) {
         boolean result = false;
         char[] chars = {'a', 'e', 'i', 'o', 'u'};      
@@ -26,6 +43,13 @@ public class WordPlay {
     }
     
     //             *** TESTER METHOD ***
+    public void testEmphasize() {
+        System.out.println("First test case:");
+        System.out.println("    " + emphasize("dna ctgaaactga", 'a'));
+        System.out.println("Second test case:");
+        System.out.println("    " + emphasize("Mary Bella Abracadabra", 'a'));
+    }
+    
     public void testReplaceVowels() {
         System.out.println(replaceVowels("Hello World", '*'));
     }
