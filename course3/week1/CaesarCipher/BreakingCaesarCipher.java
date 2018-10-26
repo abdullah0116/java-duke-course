5d9044a38e2a33a83f2c6b32c5a899bc0cd38025
Abdullah Kareem <akasak56@gmail.com>
2018-10-25 23:36:12 -0600

    new textFingerPrint method withe its tester method

--- course3/week1/CaesarCipher/BreakingCaesarCipher.java
+++ course3/week1/CaesarCipher/BreakingCaesarCipher.java
@@ -7,6 +7,21 @@ public class BreakingCaesarCipher {
             System.out.println("Key: " + i + " String: " + result);
         }
     }
+    
+    public void textFingerPrint(String string) {
+        String alpha = "abcdefghijklmnopqrstuvwxyz";
+        int[] counters = new int[26];
+        for(int i = 0; i < string.length(); i ++) {
+            char currCh = string.charAt(i);
+            int index = alpha.indexOf(Character.toLowerCase(currCh));
+            if (index != -1) {
+                counters[index] += 1;
+            }
+        }
+        for (int i = 0; i < counters.length; i++) {
+            System.out.println(alpha.charAt(i) + "\t" + counters[i]);
+        }
+    }
     //                  ***  TESTER METHODS  ***
     public void eyeBallDecryptTester() {
         // string 
@@ -16,4 +31,7 @@ public class BreakingCaesarCipher {
         eyeBallDecrypt("Pi cddc qt xc iwt rdcutgtcrt gddb lxiw ndjg wpi dc udg p hjgegxht epgin. NTAA ADJS!");
     }
     
+    public void testTextFingerPrint() {
+        textFingerPrint("At noon be in the conference room with your hat on for a surprise party. YELL LOUD!");
+    }
 }
