893b4fb665e15f38e30f8c04e9fbefe451117908
Abdullah Kareem <akasak56@gmail.com>
2018-11-09 19:13:44 -0700

    tester method.

--- course3/week3/logs/Tester.java
+++ course3/week3/logs/Tester.java
@@ -10,9 +10,17 @@ import java.util.*;
 
 public class Tester
 {
+    public void testUniqueIP() {
+        LogAnalyzer la = new LogAnalyzer();
+        la.readFile("short-test_log");
+        int uniqueIPs = la.countUniqueIPs();
+        System.out.println("There are: " + uniqueIPs + " Unique IPs.");
+    }
+    
     public void testLogEntry() {
         LogEntry le = new LogEntry("1.2.3.4", new Date(), "example request", 200, 500);
         System.out.println(le);
+        
         LogEntry le2 = new LogEntry("1.2.100.4", new Date(), "example request 2", 300, 400);
         System.out.println(le2);
     }
