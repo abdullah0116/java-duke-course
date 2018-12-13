0b8ac9a42e78292c0b03ad15d272eb09f1831ec4
Abdullah Kareem <akasak56@gmail.com>
2018-12-13 13:16:22 -0700

    finished indexOfLargest method.

--- course4/week1/SearchingEarthquakeDataStarterProgram/LargestQuakes.java
+++ course4/week1/SearchingEarthquakeDataStarterProgram/LargestQuakes.java
@@ -4,10 +4,24 @@ public class LargestQuakes {
         EarthQuakeParser parser = new EarthQuakeParser();
         String source = "data/nov20quakedatasmall.atom";
         ArrayList<QuakeEntry> list  = parser.read(source);
-        
+        int largestMag = indexOfLargest(list);
+        //System.out.println(largestMag);
         System.out.println("Total Quakes: " + list.size());
         for (QuakeEntry qe : list) {
             //System.out.println(qe);
         }
+    }
+    
+    public int indexOfLargest(ArrayList<QuakeEntry> data) {
+        int index = 0;
+        
+        for(QuakeEntry qe : data) {
+            if (qe.getMagnitude() > index) {
+                index = data.indexOf(qe);
+            }
+        } 
+        System.out.println("Largest Magnitude " + data.get(index).getMagnitude());
+        
+        return index;
     }
 }
